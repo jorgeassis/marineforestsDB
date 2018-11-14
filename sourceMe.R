e547d141b64758b52fb002ca197cf1fd3e7d20cf
Muffins 'n' Code <jorgemfa@gmail.com>
2018-11-14 22:48:31 +0000

    Update sourceMe.R

--- sourceMe.R
+++ sourceMe.R
@@ -1,1 +1,4 @@
-a <- 1
+require(readr)
+download.file("https://github.com/jorgeassis/marineforestsDB/blob/master/Data/SeagrassesPrunned.zip?raw=true",destfile="MFTempFile.zip")
+myData <- read_csv("MFTempFile.zip")
+file.remove("MFTempFile.zip")
