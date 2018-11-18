f3b0369ac784232e5f730a42155390595a2cfb87
Bears Studio <jorgemfa@gmail.com>
2018-11-18 11:02:33 +0000

    Update sourceMe.R

--- sourceMe.R
+++ sourceMe.R
@@ -194,13 +194,25 @@ listData <- function(data,taxa,status) {
 
 # ------------------------------------------------
 
-exportData <- function(data,taxa,status,file) {
+
+exportData <- function(data,taxa,status,type,file) {
+  
+  packages.to.use <- c("sp")
+  
+  for(package in packages.to.use) {
+    if( ! package %in% rownames(installed.packages()) ) { install.packages( package ) }
+    if( package == "robis" & ! "robis" %in% rownames(installed.packages()) ) { devtools::install_github("iobis/robis") }
+    if( ! package %in% rownames(installed.packages()) ) { stop("Error on package instalation") }
+    library(package, character.only = TRUE)
+  }
   
   if( missing(taxa)) { taxa <- NULL }
   if( missing(status)) { status <- NULL }
   if( missing(file)) { stop("A filename must be provided") }
+  if( missing(type)) { type <- "csv" }
   
-  options(warn=-1)
+  if( !is.null(status) & status != "accepted" & status != "unaccepted" ) { stop("Status must be accepted or unaccepted")  }
+  if( type != "csv" & type != "shp" ) { stop("Status must be csv or shp")  }
   
   if( ! is.null(status) ) {  
     
@@ -208,7 +220,7 @@ exportData <- function(data,taxa,status,file) {
       
       if( length(which(dataset$acceptedSpeciesName == taxa)) == 0 ) { stop("Taxa not found in dataset") }
       data <- data[ which(data$acceptedSpeciesName == taxa) ,] 
-      }
+    }
     if( status != "accepted" ) { 
       
       if( length(which(dataset$speciesName == taxa)) == 0 ) { stop("Taxa not found in dataset") }
@@ -216,9 +228,19 @@ exportData <- function(data,taxa,status,file) {
     
   }
   
-  write.csv(data,file=file,sep = "\t", na = "NA", dec = ".", row.names = FALSE, col.names = TRUE)
-  
+  if(type == "csv") {
+    write.csv(data,file=file,sep = "\t", na = "NA", dec = ".", row.names = FALSE, col.names = TRUE)
+    
+  }
+  if(type == "shp") {
+    
+    main.results.sp <- SpatialPointsDataFrame(data.frame(Lon=as.numeric(as.character(data$decimalLongitude)),Lat=as.numeric(as.character(data$decimalLatitude))),data)   
+    crs(main.results.sp) <- "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0" 
+    writeOGR(main.results.sp, "/", file, driver="ESRI Shapefile",overwrite_layer=TRUE)
+    
+  }
 }
+
 
 # ------------------------------------------------
 
